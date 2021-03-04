80f847d5decd491b24270f1dbf9adad1f128f37d
your name here <joshuaebrani@gmail.com>
2021-03-04 15:32:35 -0500

    Reduc cycle

--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import configureStore from './store/store'
+import {
+    receiveTodos,
+    receiveTodo,
+} from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
+    let store = configureStore()
+    window.store = store
     ReactDOM.render(<Root />, document.getElementById('main'))
+    window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
 })
